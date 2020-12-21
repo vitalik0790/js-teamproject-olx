e38a407abc4d67baa1003aa6538b60b609e50c43
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-12-21 15:44:09 +0200

    search

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,5 +1,5 @@
 const headerInput = document.querySelector('.header__input');
-console.dir(headerInput);
+// console.dir(headerInput);
 
 const shwoValue = (e) => {
     const query = e.target.value;
