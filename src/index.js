629c19ecd80305292f893b5bba0c0bf73403947a
Vova Rabeshko <rabeshko23@gmail.com>
2020-12-21 22:37:24 +0200

    footer

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import '../node_modules/modern-normalize/modern-normalize.css';
 import './styles.scss';
 import { testAuth } from './js/components/authentication';
 testAuth();
+import './js/components/footer/footer'
+import './js/components/students-modal/students-modal'
