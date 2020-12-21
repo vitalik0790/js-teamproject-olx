f3e721600c38a27f22f61093bbe0261be70dfbe8
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-12-21 19:07:13 +0200

    search

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -3,11 +3,28 @@ import { data } from '../data/data'
 const headerInput = document.querySelector('.header-logo__input');
 const headerInputTablet = document.querySelector('.header-logo__input-tablet');
 
+let query = "";
+
 const showValue = (e) => {
-    const query = e.target.value;
+    query = e.target.value;
+    console.log(query);
 }
 
 headerInput.addEventListener('input', showValue)
 headerInputTablet.addEventListener('input', showValue)
 
-const inputSearch = 
+const fetchGoods = (query) => {
+    const URL = `${BASE_URL}/call`;
+    return fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+
+}
+
+
+
+
+
+
