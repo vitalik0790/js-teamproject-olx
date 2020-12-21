0bb7134993dd0c57e1b3d7b292e3f915b2dea591
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-12-21 22:13:06 +0200

    search

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,27 +1,47 @@
 import { data } from '../data/data'
+import searchCard from '../../templates/search.hbs'
+// import axios from 'axios';
+
 
 const headerInput = document.querySelector('.header-logo__input');
 const headerInputTablet = document.querySelector('.header-logo__input-tablet');
+const searchGallery = document.querySelector('.search-gallery')
+// console.dir(headerInput);
 
-let query = "";
-
-const showValue = (e) => {
-    query = e.target.value;
-    console.log(query);
-}
-
-headerInput.addEventListener('input', showValue)
-headerInputTablet.addEventListener('input', showValue)
-
-const fetchGoods = (query) => {
-    const URL = `${BASE_URL}/call`;
-    return fetch(URL)
+export const getSearchQuery = (query) => {
+    return fetch(`${data.baseURL}/call/find?search=${query}`)
         .then(response => response.json())
-        .then(data => {
-            return data;
-        })
+        .then(data => data);
+};
 
+export const updateMarkup = (goods) => {
+    const searchMarkup = searchCard(goods);
+    // searchGallery.insertAdjacentHTML('beforeend', searchMarkup)
+    searchGallery.innerHTML = searchMarkup;
 }
+
+let inputValue = '';
+
+const getBySearch = (event) => {
+    event.preventDefault();
+    searchGallery.innerHTML = '';
+    inputValue = event.target.value;
+    // console.log(inputValue);
+
+    if (inputValue.length >= 1) {
+        getSearchQuery(inputValue)
+            .then(goods => {
+                updateMarkup(goods)
+            })
+    }
+}
+
+
+headerInput.addEventListener('input', getBySearch);
+headerInputTablet.addEventListener('input', getBySearch);
+
+
+
 
 
 
