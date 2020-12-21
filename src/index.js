66a225ccebe3f419e83e0f48184ce170267b9605
DmytroLukianenko <71637913+DmytroLukianenko@users.noreply.github.com>
2020-12-21 10:45:36 +0100

    - isLogin() - testAuth() in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './styles.scss';
 import { testAuth } from './js/components/authentication';
-import { isLogin } from './js/components/navigation-estimates'
+
 testAuth();
-// ======================================================================================
-
-
-isLogin()
-testAuth()
-
-// // console.log(document.forms.signUpForm);
-// refs.signInBtn.addEventListener('click', signInHandler)
--- src/js/components/navigation-estimates.js
+++ src/js/components/navigation-estimates.js
@@ -1,6 +1,6 @@
 import navigationEstimate from '../../templates/navigationSignIn.hbs'
 import navSignUp from '../../templates/navigationSignUp.hbs'
-// import { signInHandler, signUpHandler, logOut } from './js/components/authentication'
+
 const refs = {
     signInDiv: document.querySelector('.auth-test-btns2'),
 }
