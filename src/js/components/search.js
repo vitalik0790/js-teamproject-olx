5e5c8996760ec5557780174eb6ccc9b9d02a9793
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-12-22 17:44:40 +0200

    searchhhh

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,12 +1,11 @@
 import { data } from '../data/data'
 import searchCard from '../../templates/search.hbs'
-// import axios from 'axios';
 
 
 const headerInput = document.querySelector('.header-logo__input');
 const headerInputTablet = document.querySelector('.header-logo__input-tablet');
-const searchGallery = document.querySelector('.search-gallery')
-// console.dir(headerInput);
+const main = document.querySelector('.main')
+
 
 export const getSearchQuery = (query) => {
     return fetch(`${data.baseURL}/call/find?search=${query}`)
@@ -17,14 +16,15 @@ export const getSearchQuery = (query) => {
 export const updateMarkup = (goods) => {
     const searchMarkup = searchCard(goods);
     // searchGallery.insertAdjacentHTML('beforeend', searchMarkup)
-    searchGallery.innerHTML = searchMarkup;
+    main.innerHTML = `<div class="container"><ul class="search-gallery">${searchMarkup}</ul></div>`;
+
 }
 
 let inputValue = '';
 
 const getBySearch = (event) => {
     event.preventDefault();
-    searchGallery.innerHTML = '';
+    main.innerHTML = '';
     inputValue = event.target.value;
     // console.log(inputValue);
 
@@ -36,9 +36,22 @@ const getBySearch = (event) => {
     }
 }
 
+const onPressEnterSearch = event => {
+    if (event.code === 'Enter') {
+        if (headerInput.value.length >= 1 && headerInputTablet.value.length >= 1) {
+            getSearchQuery(inputValue)
+                .then(goods => {
+                    updateMarkup(goods);
+                })
+                .catch(error => console.log(error));
+        }
+    }
+};
 
-headerInput.addEventListener('input', getBySearch);
-headerInputTablet.addEventListener('input', getBySearch);
+
+headerInput.addEventListener('change', getBySearch);
+headerInputTablet.addEventListener('change', getBySearch);
+document.addEventListener('keydown', onPressEnterSearch);
 
 
 
