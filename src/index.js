0e39d508736a8c68b12239db8e9dc451ada02137
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2020-12-22 12:53:20 +0200

    added modalForm

--- src/index.js
+++ src/index.js
@@ -6,54 +6,60 @@ import { refs, openAdvModal } from './js/components/newAdvForm';
 testAuth();
 
 
-const images = [];
-
-const toDataURL = (element) => {
-    return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsDataURL(element.files[0]);
-    });
-};
 
 
 
-const createItemMarkup = (image) => {
-    return `
-    <li>
-    <img src=${image} alt=""/>
-    </li>
-    `;
-};
 
 
-const listMarkup = () => {
-    return `
-        <ul>
-        ${images.reduce((acc, image) => {
-        acc += createItemMarkup(image);
-        return acc;
-    }, "")}
-        </ul>
-        ${
-        images.length < 3
-            ? `<input class="image" type="file" data-index=${images.length}>`
-            : ""
-        } 
-        `;
-};
+// ==========================
+// const images = [];
+
+// const toDataURL = (element) => {
+//     return new Promise((resolve) => {
+//         const reader = new FileReader();
+//         reader.onloadend = () => resolve(reader.result);
+//         reader.readAsDataURL(element.files[0]);
+//     });
+// };
 
 
-const content = document.querySelector(".adv-content");
-content.innerHTML = listMarkup();
 
-const addImage = () => {
-    const input = document.querySelector(".image");
-    toDataURL(input)
-        .then((data) => images.push(data))
-        .then(() => (content.innerHTML = listMarkup()));
-};
+// const createItemMarkup = (image) => {
+//     return `
+//     <li>
+//     <img src=${image} alt=""/>
+//     </li>
+//     `;
+// };
 
-const addButton = document.querySelector('form-input-file');
-content.innerHTML = listMarkup();
-addButton.addEventListener('click', addImage)
+
+// const listMarkup = () => {
+//     return `
+//         <ul>
+//         ${images.reduce((acc, image) => {
+//         acc += createItemMarkup(image);
+//         return acc;
+//     }, "")}
+//         </ul>
+//         ${
+//         images.length < 3
+//             ? `<input class="image" type="file" data-index=${images.length}>`
+//             : ""
+//         } 
+//         `;
+// };
+
+
+// const content = document.querySelector(".adv-content");
+// content.innerHTML = listMarkup();
+
+// const addImage = () => {
+//     const input = document.querySelector(".image");
+//     toDataURL(input)
+//         .then((data) => images.push(data))
+//         .then(() => (content.innerHTML = listMarkup()));
+// };
+
+// const addButton = document.querySelector('form-input-file');
+// content.innerHTML = listMarkup();
+// addButton.addEventListener('click', addImage)
--- src/js/components/newAdvForm.js
+++ src/js/components/newAdvForm.js
@@ -1,93 +1,94 @@
 import newAdvForm from '../../templates/newAdvForm.hbs';
+import firstInput from '../../templates/firstInput.hbs';
+import secondInput from '../../templates/secondInput.hbs';
+import image from '../../templates/image.hbs';
+import { openInModal, closeModal } from './modal';
+import axios from 'axios';
 
+
+const baseURL = 'https://callboard-backend.herokuapp.com/call'
+
+// =================================================================================
 const refs = {
     openModalBtn: document.getElementById('button'),
     modalAdv: document.querySelector('.advert-backdrop'),
     closeModalBtn: document.querySelector('.modal-button-icone'),
     contentAdvForm: document.querySelector('.contentAdvForm'),
+    formAdv: document.forms.advForm,
+}
+
+// =========================== input Img ===========================================
+const toDataURL = (element) => {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result);
+        reader.readAsDataURL(element.files[0]);
+    });
+};
+
+// ============================= Modal ===================================
+const newAdv = {
+    title: "",
+    file: [],
+    description: "",
+    category: "",
+    price: "",
+    phone: "",
+
+}
+const postNewAdv = async (e) => {
+    e.preventDefault();
+    const result = await axios.post(baseURL, { ...newAdv, price: Number(newAdv.price) })
+    console.log(result);
+}
+const getDataForm = (e) => {
+    if (e.target.type === "file") {
+        return;
+    }
+    const { name, value } = e.target;
+    newAdv[name] = value;
+    console.log(newAdv);
+}
+
+const createBox = () => {
+    const inputWrapper = document.querySelector('.input-wrapper');
+    inputWrapper.innerHTML = '';
+    newAdv.file.forEach(img => inputWrapper.insertAdjacentHTML('beforeend', `<img src=${img} alt="" class ="img-adv-box" name="file">`))
+    if (newAdv.file.length < 6) {
+        inputWrapper.insertAdjacentHTML('beforeend', firstInput())
+        const input = document.querySelector('.file-loader-input');
+        input.addEventListener('change', async (e) => {
+            await toDataURL(e.target)
+                .then(data => newAdv.file.push(data))
+            createBox();
+
+        })
+    }
+    for (let i = newAdv.file.length; i < 5; i += 1) {
+        inputWrapper.insertAdjacentHTML('beforeend', secondInput())
+    }
+
 }
 
 const openAdvModal = (e) => {
-    refs.contentAdvForm.innerHTML = newAdvForm();
-    // refs.contentAdvForm.insertAdjacentHTML("beforeend", newAdvForm)
-    refs.modalAdv.classList.remove('is-hidden');
-    refs.closeModalBtn.addEventListener('click', closeAdvModal);
-    // addEventListener('keydown', closeAdvModal)
-    // if (e.code === 'Escape') closeAdvModal();
-}
-const closeAdvModal = () => {
-    refs.modalAdv.classList.add('is-hidden');
-    refs.contentAdvForm.innerHTML = '';
+    openInModal(newAdvForm());
+    createBox();
+    refs.formAdv = document.forms.advForm;
+    refs.formAdv.addEventListener("input", getDataForm);
+    refs.formAdv.addEventListener('submit', postNewAdv);
 }
 
 refs.openModalBtn.addEventListener('click', openAdvModal);
 
+
+
+// =================================================================
+
+
 export { refs, openAdvModal }
 
 
-// ===================================================
-// const images = [];
-
-// const toDataURL = (element) => {
-//     return new Promise((resolve) => {
-//         const reader = new FileReader();
-//         reader.onloadend = () => resolve(reader.result);
-//         reader.readAsDataURL(element.files[0]);
-//     });
-// };
-
-// const createItemMarkup = (image) => {
-//     return `
-//     <li>
-//     <img src=${image} alt=""/>
-//     </li>
-//     `;
-// };
 
 
-// const listMarkup = () => {
-//     return `
-//         <ul>
-//         ${images.reduce((acc, image) => {
-//         acc += createItemMarkup(image);
-//         return acc;
-//     }, "")}
-//         </ul>
-//         ${
-//         images.length < 3
-//             ? `<input class="image" type="file" data-index=${images.length}>`
-//             : ""
-//         } 
-//         `;
-// };
 
-
-// const content = document.querySelector(".content");
-// content.innerHTML = listMarkup();
-
-// const addImage = () => {
-//     const input = document.querySelector(".image");
-//     toDataURL(input)
-//         .then((data) => images.push(data))
-//         .then(() => (content.innerHTML = listMarkup()));
-// };
-
-// const addButton = document.querySelector('form-input-file');
-// addButton.addEventListener('click', addImage)
-
-
-//     // < !DOCTYPE html>
-//     //     <html lang="en">
-//     //         <head>
-//     //             <meta charset="UTF-8" />
-//     //             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     //             <title>Document</title>
-//     //         </head>
-//     //         <body>
-//     //             <div class="content"></div>
-//     //             <button class="addButton">Add new photo</button>
-
-//     //             <script src="./index.js"></script>
-//     //         </body>
-//     //     </html>
 
