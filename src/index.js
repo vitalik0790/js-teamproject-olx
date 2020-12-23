c8f6ecdbf2be2ff332a8203b6b35e4e5588a5b3b
Viktor Gorokhov <patronusv@gmail.com>
2020-12-23 22:23:23 +0200

    добавляет динамическую отрисовку main

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import '../node_modules/modern-normalize/modern-normalize.css';
 //import { renderHeader } from './js/components/header';
 import './styles.scss';
 import { refs, openAdvModal } from './js/components/newAdvForm';
-
+import {createMain} from './js/components/createMain'
 // const addButton = document.querySelector('form-input-file');
 // content.innerHTML = listMarkup();
 // addButton.addEventListener('click', addImage)
 
 import { sliderGallery } from './js/components/sliderGallery';
 
-import './js/components/hero';
+import {createHero} from './hero_template';
 import { isLogin } from './js/components/navigation-estimates';
 //renderHeader();
-
-isLogin()
-
+import {init} from './js/api/galleryApi'
+const initialisation = async () => {
+    await createMain()
+    await createHero()
+    await init()
+    await isLogin()
+    
+}
+initialisation()
 import { sandwichmenu } from './js/components/sandwichmenu';
-import { newAdv } from './js/components/newAdvButton';
+import { newAdv } from './js/components/newAdvButton';import hero_template from './hero_template';
+
--- src/js/api/galleryApi.js
+++ src/js/api/galleryApi.js
@@ -1,41 +1,43 @@
 import axios from 'axios';
 import {data} from '../data/data'
-const properties = {
-    width: 0,
-    height: 0,
-    isMobile: false,
-    isTablet: false,
-    isDesktop: false,
-  }
+
+// const properties = {
+//     width: 0,
+//     height: 0,
+//     isMobile: false,
+//     isTablet: false,
+//     isDesktop: false,
+//   }
   const getViewport = () => {
-    properties.width = window.screen.width
-    properties.height = window.screen.height
-    if (properties.width <= 767) {
-      properties.isMobile = true;
-    } else if (properties.width >= 768 && properties.width <= 1279) {
-      properties.isTablet = true;
-    } else properties.isDesktop = true;
-    console.log(properties);
-  }
-  const cardsToLoad = () => {
-    if (properties.isMobile) {
-      return 1
-    } else if (properties.isTablet){
-      return 2
-    } else return 4
+    data.properties.width = window.screen.width
+    data.properties.height = window.screen.height
+    if (data.properties.width <= 767) {
+      data.properties.isMobile = true;
+    } else if (data.properties.width >= 768 && data.properties.width <= 1279) {
+      data.properties.isTablet = true;
+    } else data.properties.isDesktop = true;
+    // console.log(data.properties);
   }
   
-  const loadMoreBtn = document.querySelector('.load-more')
+  // const cardsToLoad = () => {
+  //   if (properties.isMobile) {
+  //     return 1
+  //   } else if (properties.isTablet){
+  //     return 2
+  //   } else return 4
+  // }
+  
+  
 //   const loggerBtn = document.querySelector('.logger')
   const baseURL = 'https://callboard-backend.herokuapp.com';
   // let categories = [];
-  let russianCategories = [];
+  // let russianCategories = [];
   let categoriesShown = 0;
   const createMarkup = async (array, num) => { 
-    if (!russianCategories.length)  {
-      console.log(russianCategories);
+    if (!data.russianCategories.length)  {
+      console.log(data.russianCategories);
      await getRussianCategories()
-      console.log(russianCategories);
+      console.log(data.russianCategories);
     }
     let acc = ''
     for (let i = 0; i < (array.length < num? array.length : num); i +=1){
@@ -55,7 +57,7 @@ const properties = {
     const list = `
       <li class="gallery__list">
           <div class="gallery__info">
-            <h2 class="gallery__info-name">${russianCategories[indexOfCategory]}</h2>
+            <h2 class="gallery__info-name">${data.russianCategories[indexOfCategory]}</h2>
             <a class="gallery__link_view-all" href="#">Смотреть все</a>
           </div>          
           <ul class="products js-slider">               
@@ -71,45 +73,89 @@ const properties = {
   const getCategories = async () => {
     await  axios.get(`${baseURL}/call/categories`).then(response => {
       data.categories = [...response.data];
-       console.log( data.categories);
+      //  console.log( data.categories);
     })
   }
 
   const getRussianCategories = async () => {
     await  axios.get(`${baseURL}/call/russian-categories`).then(response => {
-       russianCategories = [...response.data];
-       console.log(russianCategories);
+       data.russianCategories = [...response.data];
+      //  console.log(data.russianCategories);
     })
   }
 
   const fetcherWithCounter = async (categoriesNum, cardsNum) => {
     if (!data.categories.length)  {
-      console.log(data.categories);
+      // console.log(data.categories);
      await getCategories()
-      console.log(data.categories);
+      // console.log(data.categories);
     }  
       for (let i = 0; i<categoriesNum; i+=1) {
         await axios.get(`${baseURL}/call/specific/${data.categories[categoriesShown]}`).then( async response => await createMarkup(response.data, cardsNum))      
         categoriesShown += 1;
         // console.log(categoriesShown);
         // console.log(categories.length);
-        if (categoriesShown === data.categories.length){
-          loadMoreBtn.disabled = true;
-      }
-    }    
+      //   if (categoriesShown === data.categories.length){
+      //     loadMoreBtn.disabled = true;
+      // }
+    }   
+    $(document).ready(function () {
+      $('.js-slider').slick({
+        dots: true,
+        variableWidth: true,
+      });
+    }); 
   }
+  // const loadMoreBtn = document.querySelector('.load-more')
   
   
   export const init = async () =>{
-    getViewport()  
-    await getCategories()
-    await getRussianCategories()
-    await fetcherWithCounter(3, cardsToLoad())
     
-  }
-//   init()
-  
-  loadMoreBtn.addEventListener('click', ()=>fetcherWithCounter(1, cardsToLoad()))
+    const loadMoreBtn = document.querySelector('.load-more')   
+    const fetcherWithCounter = async (categoriesNum, cardsNum) => {
+      if (!data.categories.length)  {
+        // console.log(data.categories);
+        await getCategories()
+        // console.log(data.categories);
+      }  
+      for (let i = 0; i<categoriesNum; i+=1) {
+        await axios.get(`${baseURL}/call/specific/${data.categories[categoriesShown]}`).then( async response => await createMarkup(response.data, cardsNum)).catch(error => console.log(error))      
+        categoriesShown += 1;
+          // console.log(categoriesShown);
+          // console.log(data.categories.length);
+          if (categoriesShown === data.categories.length){
+            loadMoreBtn.disabled = true;
+            loadMoreBtn.classList.add('is-disabled')
+          }
+        }   
+        $(document).ready(function () {
+          $('.js-slider').slick({
+            dots: true,
+            variableWidth: true,
+          });
+        }); 
+      }
+      const loadMore = async () => {
+        await fetcherWithCounter(1, 8)
+        await $('.js-slider').slick('unslick')
+        await $(document).ready(function () {
+          $('.js-slider').slick({
+            dots: true,
+            variableWidth: true,
+          });
+        }); 
+      }
+      
+      loadMoreBtn.addEventListener('click', loadMore)
+      
+      
+      getViewport()  
+      await getCategories()
+      await getRussianCategories()
+      await fetcherWithCounter(3, 8)   
+    }
+    
+  // loadMoreBtn.addEventListener('click', ()=>fetcherWithCounter(1, cardsToLoad()))
 //   loggerBtn.addEventListener('click', () => {
 //     console.log('categoriesShown :>> ', categoriesShown);
 //     console.log('categories :>> ', categories);
