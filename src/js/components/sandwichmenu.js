70ccd7b1a236129e54c5d223fdf7ebf5e39922c2
Vera Korzun <veraminich@gmail.com>
2020-12-23 22:20:43 +0100

    Added filter

--- src/js/components/sandwichmenu.js
+++ src/js/components/sandwichmenu.js
@@ -1,11 +1,6 @@
 import sandwichmenu from '../../templates/sandwichMenu.hbs';
 import { clearFilter } from '../api/searchInCategory';
 
-const sandwichMenu = document.getElementById('sandwichmenu');
-const getJsMenu = document.querySelector('.js_menu');
-const getSvgMenu = document.getElementById('svgMenu');
-
-
 export const func = (e) => {
   e.preventDefault();
   getJsMenu.classList.toggle("activ");
@@ -28,27 +23,57 @@ const fetchCategories = () => {
 };
 
 export const renderFilter = () => {
-  fetchCategories().then(data => {
-    renderCategories(data);
-  });
+  if (!isCategoriesShown) {
+    fetchCategories().then(data => {
+      renderCategories(data);
+      data.forEach(category => {
+        document.getElementById(category).addEventListener('click', selectCategory);
+      });
+    });
+    isCategoriesShown = true;
+  } else {
+    clearCategories();
+    isCategoriesShown = false;
+  }
 };
+const clearCategories = () =>{
+    getUl.innerHTML = '';
+}
+
+const selectCategory = (e) => {
+  e.preventDefault();
+  if (selectedCategory.length === 0) {
+    document.getElementById(e.target.id).classList.add('selected');
+    selectedCategory = e.target.id;
+  } else {
+    document.getElementById(selectedCategory).classList.remove('selected');
+    document.getElementById(e.target.id).classList.add('selected');
+    selectedCategory = e.target.id;
+  }
+}
 
 const renderCategories = categories => {
-  const getUl = document.getElementById('categoriesList');
-  getUl.innerHTML = '';
+  clearCategories();
   categories.forEach(category => { 
     getUl.innerHTML += `
-    <li class="filter__list-item">
-      <button class="filter__list-item_btn">
+    <li class="filter__list-item" >
+      <button class="filter__list-item_btn" id="${category}">
         ${category}
       </button>
     </li>
     `; 
   });
-  getUl.addEventListener('click', activeCategory);
 };
 
+const sandwichMenu = document.getElementById('sandwichmenu');
+const getJsMenu = document.querySelector('.js_menu');
+const getSvgMenu = document.getElementById('svgMenu');
+let isCategoriesShown = false;
+let selectedCategory = '';
+
 createMarkUp();
+
+const getUl = document.getElementById('categoriesList');
 
 sandwichMenu.addEventListener('click', func); 
 getSvgMenu.addEventListener('click', func);
