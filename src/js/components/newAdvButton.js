d69ac882686a74cf807d3739e83ed08053b52e04
Vera Korzun <veraminich@gmail.com>
2020-12-23 13:09:33 +0100

    Added changes

--- src/js/components/newAdvButton.js
+++ src/js/components/newAdvButton.js
@@ -1,28 +1,17 @@
-//==============================================
-// Элемент вызывает событие открытия модального окна создания объявления.
-// Создать объявление может только зарегистрированный пользователь.
-// В противном случае, при клике на кнопку реализовано перенаправление на форму регистрации
-import {getToken} from '../utils/getToken';
+import { getToken } from '../utils/getToken';
 import { openInModal, closeModal, inCurrentModal } from './modal';
 import signUpFormTemplate from '../../templates/signUpFormTemplate.hbs';
-import {
-  signUpHandler,
-  signInHandler,
-  logOut,
-  testAuth,
-} from './authentication';
+import { signUpHandler} from './authentication';
+
 const newAdvbutton = document.querySelector('.header-btn-adv');
 
-
 export const newAdv = () => {
-    if (!localStorage.getItem('accessToken')) {
+    if (!getToken()) {
         signUpHandler();
-        console.log('Hello');
+        //console.log('Hello');
     } else {
         console.log('Good bye');
     }
-
-
 };
 
 newAdvbutton.addEventListener('click', newAdv);
