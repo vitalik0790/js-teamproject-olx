0b30cf47af3926158c2073df359c416c9c2b5f28
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2020-12-23 14:34:01 +0200

    added markup vodal

--- src/js/components/newAdvForm.js
+++ src/js/components/newAdvForm.js
@@ -1,19 +1,26 @@
 import newAdvForm from '../../templates/newAdvForm.hbs';
 import firstInput from '../../templates/firstInput.hbs';
 import secondInput from '../../templates/secondInput.hbs';
-import image from '../../templates/image.hbs';
 import { openInModal, closeModal } from './modal';
 import axios from 'axios';
 import { data } from '../data/data'
+import { getToken } from '../utils/getToken'
 
 
+// axios.defaults.headers.common['Authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmUzMWI2MWE0ZjYyYzAwMTdhZWE3ZTIiLCJzaWQiOiI1ZmUzMWI2NGE0ZjYyYzAwMTdhZWE3ZTQiLCJpYXQiOjE2MDg3MTkyMDQsImV4cCI6MTYwODcyMjgwNH0.aqHR6o_1qA-DsEvG1OFvQ9awNLm8BItSmhJBT4Y5BRc';
+const baseURL = 'https://callboard-backend.herokuapp.com/call';
+// axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 
-const baseURL = 'https://callboard-backend.herokuapp.com/call'
+const options = {
+    headers: {
+        Authorization: 'Bear eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmUzMWI2MWE0ZjYyYzAwMTdhZWE3ZTIiLCJzaWQiOiI1ZmUzMWI2NGE0ZjYyYzAwMTdhZWE3ZTQiLCJpYXQiOjE2MDg3MTkyMDQsImV4cCI6MTYwODcyMjgwNH0.aqHR6o_1qA-DsEvG1OFvQ9awNLm8BItSmhJBT4Y5BRc',
+        'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
+    }
+}
 
 // =================================================================================
 const refs = {
     openModalBtn: document.getElementById('button'),
-    modalAdv: document.querySelector('.advert-backdrop'),
     closeModalBtn: document.querySelector('.modal-button-icone'),
     contentAdvForm: document.querySelector('.contentAdvForm'),
     formAdv: document.forms.advForm,
@@ -34,14 +41,27 @@ const newAdv = {
     file: [],
     description: "",
     category: "",
-    price: "",
+    price: 0,
     phone: "",
-
 }
+
+
 const postNewAdv = async (e) => {
     e.preventDefault();
-    const result = await axios.post(baseURL, { ...newAdv, price: Number(newAdv.price) })
-    console.log(result);
+    const getAdvToken = getToken();
+    if (getAdvToken) {
+        newAdv.file = newAdv.file.join('')
+        newAdv.price = Number(newAdv.price)
+        console.log(newAdv.file);
+        try {
+            await axios.post(baseURL, { ...newAdv }, options)
+            // closeModal();
+        } catch (error) {
+            console.log("sendForm", error);
+            alert("Something went wrong. Try again")
+        }
+    }
+
 }
 const getDataForm = (e) => {
     if (e.target.type === "file") {
@@ -49,14 +69,23 @@ const getDataForm = (e) => {
     }
     const { name, value } = e.target;
     newAdv[name] = value;
-    console.log(newAdv);
 }
 
 const createBox = () => {
+    let quantityImg = 0;
+    let iFor = 0;
+    if (window.screen.width > 320 && window.screen.width < 768) {
+        quantityImg = 6;
+        iFor = 5;
+    } else {
+        quantityImg = 5;
+        iFor = 4;
+    }
     const inputWrapper = document.querySelector('.input-wrapper');
     inputWrapper.innerHTML = '';
     newAdv.file.forEach(img => inputWrapper.insertAdjacentHTML('beforeend', `<img src=${img} alt="" class ="img-adv-box" name="file">`))
-    if (newAdv.file.length < 6) {
+
+    if (newAdv.file.length < quantityImg) {
         inputWrapper.insertAdjacentHTML('beforeend', firstInput())
         const input = document.querySelector('.file-loader-input');
         input.addEventListener('change', async (e) => {
@@ -66,10 +95,9 @@ const createBox = () => {
 
         })
     }
-    for (let i = newAdv.file.length; i < 5; i += 1) {
+    for (let i = newAdv.file.length; i < iFor; i += 1) {
         inputWrapper.insertAdjacentHTML('beforeend', secondInput())
     }
-
 }
 
 const openAdvModal = (e) => {
